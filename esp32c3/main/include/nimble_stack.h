ddaeec23c1594835d7f771cccc71dc40fd0700f2
RibbeGlob <globiszpython@gmail.com>
2025-02-06 21:25:50 +0100

    esp32c3 submodule

--- esp32c3/main/include/nimble_stack.h
+++
@@ -1,9 +0,0 @@
-#ifndef NIMBLE_STACK_H
-#define NIMBLE_STACK_H
-
-void on_stack_reset(int reason);
-void on_stack_sync(void);
-void nimble_host_config_init(void);
-void nimble_host_task(void *param);
-
-#endif
